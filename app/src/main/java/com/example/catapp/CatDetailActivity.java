77566a16b8360b696e9e8996dcb29f8081ef976e
immanuel <immanuelabosh@outlook.com>
2019-11-15 18:21:43 +1100

    - Added Images - Added Cat detail page

--- app/src/main/java/com/example/catapp/CatDetailActivity.java
+++ app/src/main/java/com/example/catapp/CatDetailActivity.java
@@ -4,64 +4,96 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.catapp.Database.AppDatabase;
 import com.example.catapp.Database.Cat;
+import com.example.catapp.Database.CatImage;
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CatDetailActivity extends AppCompatActivity {
     ConstraintLayout bookConstraintLayout;
-    TextView titleTextView;
-    TextView authorTextView;
-    TextView descriptionTextView;
-    TextView rankTextView;
-    ImageView coverImageView;
+    TextView nameTextView;
+    TextView informationTextView;
+    ImageView catImageView;
+    String information;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_detail);
 
-/*
-        bookConstraintLayout = findViewById(R.id.book);
-        titleTextView = bookConstraintLayout.findViewById(R.id.tv_title);
-        authorTextView = bookConstraintLayout.findViewById(R.id.tv_author);
-        rankTextView = bookConstraintLayout.findViewById(R.id.tv_rank);
-        coverImageView = bookConstraintLayout.findViewById(R.id.iv_cover);
-        descriptionTextView = findViewById(R.id.tv_description);
-*/
+
+        informationTextView = findViewById(R.id.information);
+        catImageView = findViewById(R.id.imageView);
+        nameTextView = findViewById(R.id.name);
+
 
         Intent intent = getIntent();
 
         Cat cat = (Cat) intent.getSerializableExtra("cat");
-
-        cat.getId();
-        cat.getName();
-        cat.getAltNames();
-        cat.getDescription();
-        cat.getTemperament();
-        cat.getOrigin();
-        cat.getLifeSpan();
-        cat.getWikipediaUrl();
-        cat.getDogFriendly();
-
-        //I will get weight when I get the image
-        //cat.getMetric();
+        nameTextView.setText(cat.getName());
 
 
+        information = "\nAlt Names: ";
 
-/*
-        titleTextView.setText(book.getTitle());
-        authorTextView.setText(book.getAuthor());
-        rankTextView.setText("#" + String.valueOf(book.getRank()));
-        descriptionTextView.setText(book.getDescription());
+        if (cat.getAltNames().isEmpty()){
+            information += " none";
+        }else {
+            information += cat.getAltNames();
+        }
+        information += "\n\nDescription: "  + cat.getDescription();
+        information += "\n\nTemperament: " + cat.getTemperament();
+        information += "\n\nOrigin: " + cat.getOrigin();
+        information += "\n\nLifespan: " + cat.getLifeSpan();
+        information += "\n\nWikipedia Link: " + cat.getWikipediaUrl();
+        information += "\n\nDog Friendliness (1-5): " + cat.getDogFriendly();
 
-        String imageUrl = book.getBookImage();
-        Glide.with(this).load(imageUrl).into(coverImageView);
-*/
+        setImageAndWeight(cat.getId());
+
     }
+    public void setImageAndWeight(String breedID) {
+        String url = "https://api.thecatapi.com/v1/images/search?breed_id=" + breedID;
+        final RequestQueue requestQueue =  Volley.newRequestQueue(getApplicationContext());
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new Gson();
+                CatImage[] objectsArray = gson.fromJson(response, CatImage[].class);
+                Glide.with(getApplicationContext()).load(objectsArray[0].getUrl()).into(catImageView);
+                information += "\n\nWeight: " + objectsArray[0].getWeight() + " kg";
+                informationTextView.setText(information);
+                requestQueue.stop();
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"The request failed: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                requestQueue.stop();
+            }
+        };
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, responseListener,
+                errorListener);
+        requestQueue.add(stringRequest);
+
+    }
+
 
 }
--- app/src/main/java/com/example/catapp/Database/CatImage.java
+++ app/src/main/java/com/example/catapp/Database/CatImage.java
@@ -3,8 +3,16 @@ package com.example.catapp.Database;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class CatImage {
 
+    @SerializedName("breeds")
+    @Expose
+    private List<Breed> breeds = null;
+    @SerializedName("id")
+    @Expose
+    private String id;
     @SerializedName("url")
     @Expose
     private String url;
@@ -14,6 +22,22 @@ public class CatImage {
     @SerializedName("height")
     @Expose
     private int height;
+
+    public List<Breed> getBreeds() {
+        return breeds;
+    }
+
+    public void setBreeds(List<Breed> breeds) {
+        this.breeds = breeds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getUrl() {
         return url;
@@ -39,4 +63,40 @@ public class CatImage {
         this.height = height;
     }
 
+    public String getWeight() {
+        return breeds.get(0).weight.metric;
+    }
+    class Breed {
+
+        @SerializedName("weight")
+        @Expose
+        private Weight weight;
+
+        public Weight getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Weight weight) {
+            this.weight = weight;
+        }
+
+    }
+    class Weight {
+
+        @SerializedName("metric")
+        @Expose
+        private String metric;
+
+        public String getMetric() {
+            return metric;
+        }
+
+        public void setMetric(String metric) {
+            this.metric = metric;
+        }
+
+    }
+
+
 }
+
